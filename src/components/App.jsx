1739a2584491ca975be06c72c68ebd8e1f41286c
Paweł Witecki-Wawrzyniak <pawel.witecki.wawrzyniak@gmail.com>
2023-09-29 23:23:40 +0200

    Trying to use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  /* useEffect(() => {
+    const ls = localStorage.getItem('contacts');
+    const lsParse = JSON.parse(ls);
+    console.log('lsParse', lsParse);
+    return setContacts(lsParse);
+  }, []); */
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    let name = form.elements.name.value;
+    let number = form.elements.number.value;
+    const id = nanoid();
+
+    const listOfContacts = contacts.map(contact => contact.name);
+
+    if (listOfContacts.find(contact => contact === name)) {
+      return alert(name + ' is already in your contacts!');
+    }
+    setContacts([...contacts, { name, number, id }]);
+
+    form.reset();
+    /* localStorage.setItem('contacts', JSON.stringify(contacts)); */
+  };
+
+  const handleFilter = event => {
+    const filteredName = event.currentTarget.value;
+    setFilter(filteredName.toLowerCase());
+  };
+
+  const handleDelete = event => {
+    const id = event.currentTarget.id;
+    const index = contacts.findIndex(contact => contact.id === id);
+    const arrOfContacts = contacts;
+    arrOfContacts.splice(index, 1);
+    setContacts(arrOfContacts);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <ContactFilter handleFilter={handleFilter} />
+      <ContactList
+        contacts={contacts}
+        filterPhrase={filter}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
